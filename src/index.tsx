67c367aefa3d6ce5a0d03a729d9b5295ee988b30
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2022-05-20 21:01:26 -0300

    refactor: organize structure

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { ApplicationRoutes } from "./routes";
 import "./index.css";
-import { GameProvider } from "./contexts/game-context";
+import { GameProvider } from "./contexts";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
